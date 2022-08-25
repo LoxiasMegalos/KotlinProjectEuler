b8d0d2e05e5e63465061d6805ed5c6c3c027de0d
LoxiasMegalos <murillo.alkantara@gmail.com>
2022-08-25 20:25:19 -0300

    Melhorando Problema 1 em uma classe

--- ExerciciosEuler/src/main/kotlin/P1MultiplosDe3e5.kt
+++ ExerciciosEuler/src/main/kotlin/P1MultiplosDe3e5.kt
@@ -1,4 +1,6 @@
-fun main(){
+class P1MultiplosDe3e5(
+    private var numero:Int
+) {
 
     /*
     If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
@@ -6,18 +8,30 @@ fun main(){
     Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
      */
 
+    private var soma = 0
+    private var calculoRealizado = false
 
-    var soma = 0
-
-    print("Digite um numero: ")
-    val numero = readln().toInt()
-
-    for(i in 1 until numero){
-        if(i % 3 == 0 || i % 5 == 0){
-            soma += i
+    fun resultado():String{
+        if(calculoRealizado){
+            return "A soma dos multiplos de 3 e 5 abaixo de $numero é: $soma"
         }
+        throw Exception ("Calcule ao menos uma vez o resultado para que obtenha o resultado")
     }
 
-    println("A soma dos multiplos de 3 e 5 abaixo de $numero é: $soma")
+    fun calculaResultado(){
+        for(i in 1 until numero){
+            if(i % 3 == 0 || i % 5 == 0){
+                soma += i
+            }
+        }
+        calculoRealizado = true
+    }
 
+    fun escolheNovoNumero(novoNumero: Int){
+        numero = novoNumero
+    }
+
+    fun getNumero():Int{
+        return numero
+    }
 }
